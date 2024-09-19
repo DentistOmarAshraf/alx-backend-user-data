6fcb90c5d9ab1f0fcbf873436898d62510e0183a
DentistOmarAshraf <omar_ashraf@live.com>
2024-09-19 05:59:33 +0300

    ----

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Class BasicAuth
 """
 from flask import request
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -23,3 +24,20 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """decode_base64_authorization_header
+        """
+        if not base64_authorization_header:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            base = base64_authorization_header.encode('utf-8')
+            decoded = base64.b64decode(base)
+            info = decoded.decode('utf-8')
+            return info
+        except Exception:
+            return None
