4b0fc308e80b051e3de369ebc54361fa29045d4e
DentistOmarAshraf <omar_ashraf@live.com>
2024-09-19 05:33:42 +0300

    ---

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """
-Class basic auth
+Class BasicAuth
 """
 from flask import request
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
+    """
+    Class BasicAuth
+    """
     pass
