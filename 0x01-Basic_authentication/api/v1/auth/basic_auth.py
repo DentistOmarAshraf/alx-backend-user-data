d92032aeb6d206c3ef58395eda449a1ea87676eb
DentistOmarAshraf <omar_ashraf@live.com>
2024-09-19 05:44:44 +0300

    basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,16 @@ class BasicAuth(Auth):
     """
     Class BasicAuth
     """
-    pass
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """extract_base64_authorization_header
+        """
+        if not authorization_header:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
