15e4730f7a0e9b9b1bc2737a2c6c0730cf589aeb
DentistOmarAshraf <omar_ashraf@live.com>
2024-09-20 04:07:08 +0300

    basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,9 +3,10 @@
 Class BasicAuth
 """
 from flask import request
+from models.user import User
 from api.v1.auth.auth import Auth
 import base64
-from typing import Tuple
+from typing import Tuple, TypeVar
 
 
 class BasicAuth(Auth):
@@ -56,3 +57,21 @@ class BasicAuth(Auth):
             return (None, None)
         result = decoded_base64_authorization_header.split(':')
         return (result[0], result[1])
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str) -> TypeVar('User'):
+        """User_object_from credentials
+        """
+        if not user_email or not isinstance(user_email, str):
+            return None
+        if not user_pwd or not isinstance(user_pwd, str):
+            return None
+        list_of_user = User.search({'email': user_email})
+        if not list_of_user:
+            return None
+        the_user: User = list_of_user[0]
+        if not the_user.is_valid_password(user_pwd):
+            return None
+        return the_user
