cf45da181f2c3d9e5133f9813ad32ed1c21d0960
DentistOmarAshraf <omar_ashraf@live.com>
2024-09-22 09:05:45 +0300

    [edit] more efficient

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,7 +5,7 @@ Class Auth
 from flask import request
 from typing import TypeVar, List
 from os import path as PATH
-import fnmatch
+from fnmatch import fnmatch
 
 
 class Auth:
@@ -22,10 +22,9 @@ class Auth:
         if not path or not excluded_paths:
             return True
         path_to_check = PATH.normpath(path)
-        paths = [PATH.normpath(p) for p in excluded_paths]
         check = False
-        for path in paths:
-            if fnmatch.fnmatch(path_to_check, path):
+        for path in excluded_paths:
+            if fnmatch(path_to_check, PATH.normpath(path)):
                 check = True
                 break
         if check:
